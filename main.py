27bebe024ad901a8c7605f4b0050027670fd6ba3
yeeeeeeeeeeengyu <153285299+yeeeeeeeeeeengyu@users.noreply.github.com>
2024-11-02 11:44:54 +0900

    testing

---
+++ main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from enum import Enum # 리스트를 딕셔너리처럼 집합으로 만들어줌 >> 직관적임
+from pydantic import BaseModel # 데이터 유효성 검사, 타입 힌트 강제, 오토캐스팅, 코드 간결화
+
+
+class Item(BaseModel): 
+    name : str
+    desc : str | None = None
+    price : float
+    tax : float | None = None
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "INGYUWORLD HI"}
+
+@app.post("/items/")
+async def create_item(item : Item):
+    return item
+
+@app.get("/user/{user_id}")
+async def user(user_id : str):
+    if user_id == "me":
+        return {"Current User" : "킹갓최인규"}
+    else:
+        return {"Current User" : user_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def readfile(file_path : str):
+    return {"File_path" : file_path, "Message" : "씨발"}
+
+@app.get("/jot")
+async def jot(skip : int = 0, limit : int = 10):
+    return fake_items_db[skip : skip + limit]
